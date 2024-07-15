5fccb17b911543bc78d8dcacc37d329d886696dc
Lars <cjunior036@gmail.com>
2024-07-15 16:15:00 -0300

    added cname to allowed hosts

--- e_commerce1/settings.py
+++ e_commerce1/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = ['localhost']
+ALLOWED_HOSTS = ['django-barterout1-env.eba-r6h24h2n.us-west-2.elasticbeanstalk.com']
 
 
 # Application definition
