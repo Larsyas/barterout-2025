97f56038f26fef6df63916aeb8145aeddefde75f
Lars <cjunior036@gmail.com>
2024-07-15 15:01:54 -0300

    removed wrong old files

--- e_commerce1/settings.py
+++ e_commerce1/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = ['django-barterout-env.eba-c8kek26p.us-west-2.elasticbeanstalk.com', 'localhost']
+ALLOWED_HOSTS = ['localhost']
 
 
 # Application definition
