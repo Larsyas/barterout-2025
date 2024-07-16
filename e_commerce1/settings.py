1a78ec6061dbdb11f78ec5713c1753e12a187354
Lars <cjunior036@gmail.com>
2024-07-16 15:31:09 -0300

    heroku3

--- e_commerce1/settings.py
+++ e_commerce1/settings.py
@@ -100,7 +100,6 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
-        'default': dj_database_url.config(conn_max_age=600)
     }
 }
 
